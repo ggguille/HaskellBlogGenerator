5cc131cd84c86014a321c63518f1c50bc31f7c18
Guille <g.granado.g@gmail.com>
2022-09-22 20:22:36 +0200

    3.2 Adding type signatures

--- hello.hs
+++ hello.hs
@@ -1,10 +1,28 @@
+main :: IO ()
 main = putStrLn myhtml
 
-myhtml = makeHtml "My page title" "My page content"
+myhtml :: String
+myhtml =
+    makeHtml 
+        "Hello title" 
+        (h1_ "Hello, world!" <> p_ "Let's learn about Haskell!")
 
-html_ content = "<html>" <> content <> "</html>"
-head_ content = "<head>" <> content <> "</head>"
-title_ content = "<title>" <> content <> "</title>"
-body_ content = "<body>" <> content <> "</body>"
+html_ :: String -> String
+html_ = el "html"
+head_ :: String -> String
+head_ = el "head"
+title_ :: String -> String
+title_ = el "title"
+body_ :: String -> String
+body_ = el "body"
+h1_ :: String -> String
+h1_ = el "h1"
+p_ :: String -> String
+p_ = el "p"
 
+makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+
+el :: String -> String -> String
+el tag content = 
+    "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
